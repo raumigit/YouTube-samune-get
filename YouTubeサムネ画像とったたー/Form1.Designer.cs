d55d5b6ee92bb726f89007868b1a90f39f65ea1b
raumigit <86038378+raumigit@users.noreply.github.com>
2022-07-03 00:04:23 +0900

    start
    2022_07_02_1

---
+++ YouTubeサムネ画像とったたー/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace YouTubeサムネ画像とったたー
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBoxテキスト = new System.Windows.Forms.CheckBox();
+            this.checkBoxクリップボード = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.loglist = new System.Windows.Forms.ListBox();
+            this.checkBoxAll = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "変換";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(18, 35);
+            this.textBox_input.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(257, 19);
+            this.textBox_input.TabIndex = 2;
+            this.textBox_input.Click += new System.EventHandler(this.textBox_input_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "YouTube短縮URL";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 56);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(158, 16);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "maxresdefault(一番大きい)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(18, 75);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(76, 16);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.Text = "sdddefault";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.linkLabel1.Location = new System.Drawing.Point(16, 195);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(79, 16);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "サムネ表示";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 165);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "出力";
+            // 
+            // checkBoxテキスト
+            // 
+            this.checkBoxテキスト.AutoSize = true;
+            this.checkBoxテキスト.Location = new System.Drawing.Point(18, 93);
+            this.checkBoxテキスト.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBoxテキスト.Name = "checkBoxテキスト";
+            this.checkBoxテキスト.Size = new System.Drawing.Size(108, 16);
+            this.checkBoxテキスト.TabIndex = 8;
+            this.checkBoxテキスト.Text = "テキストとして出す";
+            this.checkBoxテキスト.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxクリップボード
+            // 
+            this.checkBoxクリップボード.AutoSize = true;
+            this.checkBoxクリップボード.Location = new System.Drawing.Point(18, 112);
+            this.checkBoxクリップボード.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBoxクリップボード.Name = "checkBoxクリップボード";
+            this.checkBoxクリップボード.Size = new System.Drawing.Size(124, 16);
+            this.checkBoxクリップボード.TabIndex = 9;
+            this.checkBoxクリップボード.Text = "クリップボードに付ける";
+            this.checkBoxクリップボード.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(7, 243);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(228, 15);
+            this.progressBar1.TabIndex = 10;
+            this.progressBar1.Value = 2;
+            // 
+            // loglist
+            // 
+            this.loglist.FormattingEnabled = true;
+            this.loglist.ItemHeight = 12;
+            this.loglist.Items.AddRange(new object[] {
+            "Youtubeサムネ取得ver1.3.3",
+            ">_"});
+            this.loglist.Location = new System.Drawing.Point(271, 195);
+            this.loglist.Margin = new System.Windows.Forms.Padding(2);
+            this.loglist.Name = "loglist";
+            this.loglist.Size = new System.Drawing.Size(165, 64);
+            this.loglist.TabIndex = 11;
+            // 
+            // checkBoxAll
+            // 
+            this.checkBoxAll.AutoSize = true;
+            this.checkBoxAll.Location = new System.Drawing.Point(18, 134);
+            this.checkBoxAll.Name = "checkBoxAll";
+            this.checkBoxAll.Size = new System.Drawing.Size(105, 16);
+            this.checkBoxAll.TabIndex = 12;
+            this.checkBoxAll.Text = "全解像度を出力";
+            this.checkBoxAll.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(469, 306);
+            this.Controls.Add(this.checkBoxAll);
+            this.Controls.Add(this.loglist);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBoxクリップボード);
+            this.Controls.Add(this.checkBoxテキスト);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_input);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "YouTubeサムネ取得";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxテキスト;
+        private System.Windows.Forms.CheckBox checkBoxクリップボード;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox loglist;
+        private System.Windows.Forms.CheckBox checkBoxAll;
+    }
+}
+
---
+++ YouTubeサムネ画像とったたー/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace YouTubeサムネ画像とったたー
+{
+    public partial class Form1 : Form
+    {
+        
+        string imgURL;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (end ==true)
+            {
+                linkLabel1.LinkVisited = true;
+                System.Diagnostics.Process.Start(imgURL);
+            }
+            else
+            {
+                loglist.Items.Add("一度も変換されていません");
+                return;
+            }
+            
+        }
+        bool end = false;
+        //button
+        public void Button1_Click(object sender, EventArgs e)
+        {
+            string inurl = textBox_input.Text;
+            string convurl;
+            string maxresdefault = "https://img.youtube.com/vi/@/maxresdefault.jpg";
+            string sddfault="https://img.youtube.com/vi/@/sddefault.jpg";
+
+            //string hqdefault = "https://img.youtube.com/vi/@/hqdefault.jpg";
+            //string mqdefault = "https://img.youtube.com/vi/@/mqdefault.jpg";
+            //string defaultpic = "https://img.youtube.com/vi/@/default.jpg";
+            //
+            //string samune1 = "https://img.youtube.com/vi/@/1.jpg";
+            //string samune2 = "https://img.youtube.com/vi/@/2.jpg";
+            //string samune3 = "https://img.youtube.com/vi/@/3.jpg";
+
+            string[] arraysamune =new string[] {"https://img.youtube.com/vi/@/maxresdefault.jpg",
+            "https://img.youtube.com/vi/@/sddefault.jpg",
+            "https://img.youtube.com/vi/@/hqdefault.jpg","https://img.youtube.com/vi/@/mqdefault.jpg","https://img.youtube.com/vi/@/default.jpg",
+            "https://img.youtube.com/vi/@/1.jpg","https://img.youtube.com/vi/@/2.jpg","https://img.youtube.com/vi/@/3.jpg" };
+
+
+            progressBar1.Value = 2;//2%
+
+            //抽出
+            Task.Delay(9200);
+            progressBar1.Value = 20;//20%
+
+            //test https://youtu.be/uBn1frMI_6Q
+
+
+            bool Urlcheck = true;
+
+            if (textBox_input.Text == "")
+            {
+               loglist.Items.Add("Warn:空");
+                Urlcheck = false;
+                return;
+            }
+
+            if (textBox_input.Text.StartsWith("https://youtu.be")) { }
+            else
+            {
+                loglist.Items.Add("URLが一致しません");
+                Urlcheck = false;
+            }
+            if (Urlcheck==false)
+            {
+                return;
+            }
+
+
+            
+            try
+            {
+                convurl = inurl.Substring(17, 11);
+                loglist.TopIndex = loglist.Items.Add (convurl);
+            
+            
+                progressBar1.Value = 64;
+
+                if (radioButton1.Checked == true)//サイズ判定
+                {
+                    //convimage https://img.youtube.com/vi/uBn1frMI_6Q/maxresdefault.jpg
+                    imgURL = maxresdefault.Replace("@",convurl);
+                }
+                else if (radioButton2.Checked==true)
+                {
+                    imgURL = sddfault.Replace("@", convurl);
+                }
+                
+
+                if (checkBoxテキスト.Checked == true)
+                {
+                    if (radioButton1.Checked == true)
+                    {
+                        File.AppendAllText(@"URL.txt", imgURL+"\n");
+                    }
+                    else if (radioButton2.Checked == true)
+                    {
+                        File.AppendAllText(@"URL.txt", imgURL+"\n");
+                    }
+                }
+
+                progressBar1.Value = 80;
+
+                if (checkBoxクリップボード.Checked==true)
+                {
+                    if (radioButton1.Checked==true)
+                    {
+                        Clipboard.SetData(DataFormats.Text, imgURL);
+                    }
+                    else if(radioButton2.Checked==true)
+                    {
+                        Clipboard.SetData(DataFormats.Text, imgURL);
+                    }
+                }
+
+                if (checkBoxAll.Checked==true)
+                    if (Directory.Exists(@"全サムネイル") !=true)
+                    {
+                        Directory.CreateDirectory("全サムネイル");
+                    }
+                   
+                {
+                    for (int i = 0; i < arraysamune.Length; i++)
+                    {
+                        arraysamune[i] = arraysamune[i].Replace("@", convurl);
+
+                       
+                        File.AppendAllText(@"全サムネイル/URL.txt", arraysamune[i] + "\n");
+                    }
+                    File.AppendAllText(@"全サムネイル/URL.txt", "_____________________________________________________\n");
+                }
+                progressBar1.Value = 100;
+                end = true;
+            }
+            catch (Exception)
+            {
+                loglist.Items.Add("ERROR:変換に失敗");
+
+            }
+        }
+
+        private void textBox_input_TextChanged(object sender, EventArgs e)
+        {
+            textBox_input.SelectAll();
+        }
+
+        
+    }
+}
